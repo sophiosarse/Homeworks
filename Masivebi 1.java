1bc58f8c311e8ec387c55135d194ac733a97bf4a
sophiosarse <sofiosarsevanidze@mziuri.ge>
2024-11-05 19:43:16 +0400

    Update Masivebi1.java

--- Masivebi 1.java
+++ Masivebi 1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Masivebi1{
     public static void main(String[] args) {
         /*
 
